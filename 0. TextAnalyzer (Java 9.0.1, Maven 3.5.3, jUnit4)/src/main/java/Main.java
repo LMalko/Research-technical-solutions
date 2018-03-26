388a3a979df897a18f6c42428c514daf6fcf02f3
LMalko <lmalko2601@gmail.com>
2018-03-27 00:54:23 +0200

    Add AppController instance to Main.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Main.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Main.java
@@ -1,6 +1,9 @@
+import Controller.ApplicationController;
+
 public class Main {
 
         public static void main(String[] args) {
                 String filename = args[1];
+                ApplicationController appController = new ApplicationController(filename);
         }
 }

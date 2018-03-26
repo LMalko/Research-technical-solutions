9226b6493e4ec67ccf74e38040c7bc0d1ae8530f
LMalko <lmalko2601@gmail.com>
2018-03-27 00:50:15 +0200

    Implement IterableText in FilecContent.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Model/FileContent.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Model/FileContent.java
@@ -1,4 +1,23 @@
 package Model;
 
-public class FileContent {
+import Interface.IterableText;
+
+import java.util.Iterator;
+
+public class FileContent implements IterableText{
+
+        private String filename;
+
+        public FileContent(String filename){
+                this.filename = filename;
+        }
+        @Override
+        public Iterator<String> charIterator() {
+                return null;
+        }
+
+        @Override
+        public Iterator<String> wordIterator() {
+                return null;
+        }
 }

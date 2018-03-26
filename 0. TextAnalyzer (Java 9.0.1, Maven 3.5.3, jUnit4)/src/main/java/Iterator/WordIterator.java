cfe19588bcf4aa488c017f0f5c78014d47532fbd
LMalko <lmalko2601@gmail.com>
2018-03-27 01:12:20 +0200

    Add constructor & methods in WordIterator.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Iterator/WordIterator.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Iterator/WordIterator.java
@@ -1,4 +1,25 @@
 package Iterator;
 
-public class WordIterator {
+import Model.FileContent;
+
+import java.util.Iterator;
+
+public class WordIterator implements Iterator{
+
+        private FileContent fileContent;
+
+        public WordIterator(FileContent fileContent) {
+                this.fileContent = fileContent;
+        }
+
+
+        @Override
+        public boolean hasNext() {
+                return false;
+        }
+
+        @Override
+        public Object next() {
+                return null;
+        }
 }

f0b1c671295a702a7410e054c6776e87e257cea5
LMalko <lmalko2601@gmail.com>
2018-03-27 01:19:43 +0200

    Write methods in CharIterator.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Iterator/CharIterator.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Iterator/CharIterator.java
@@ -7,18 +7,20 @@ import java.util.Iterator;
 public class CharIterator implements Iterator{
 
         private FileContent fileContent;
+        private int index;
 
         public CharIterator(FileContent fileContent) {
                 this.fileContent = fileContent;
+                this.index = 0;
         }
 
         @Override
         public boolean hasNext() {
-                return false;
+                return this.index < fileContent.getCharsCollection().size();
         }
 
         @Override
         public Object next() {
-                return null;
+                return getCharsCollection().get(this.index++);
         }
 }

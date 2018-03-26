1483d3eee34cab1cdcde21c0806a9b18f414621e
LMalko <lmalko2601@gmail.com>
2018-03-27 01:10:54 +0200

    Add constructor & methods in CharIterator

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Iterator/CharIterator.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Iterator/CharIterator.java
@@ -1,4 +1,24 @@
 package Iterator;
 
-public class CharIterator {
+import Model.FileContent;
+
+import java.util.Iterator;
+
+public class CharIterator implements Iterator{
+
+        private FileContent fileContent;
+
+        public CharIterator(FileContent fileContent) {
+                this.fileContent = fileContent;
+        }
+
+        @Override
+        public boolean hasNext() {
+                return false;
+        }
+
+        @Override
+        public Object next() {
+                return null;
+        }
 }

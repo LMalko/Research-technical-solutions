382791b89fd647d0075fdfdce190afcdc094b6f7
LMalko <lmalko2601@gmail.com>
2018-03-27 03:10:32 +0200

    Add code for testing in Main.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Main.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Main.java
@@ -3,7 +3,10 @@ import Controller.ApplicationController;
 public class Main {
 
         public static void main(String[] args) {
-                String filename = args[1];
+
+                String filename = args[0];
+                System.out.println(filename);
                 ApplicationController appController = new ApplicationController(filename);
+                appController.print();
         }
 }

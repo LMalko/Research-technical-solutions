6a424c30a9fa240abcfc75c252cd9f5f0815ff62
LMalko <lmalko2601@gmail.com>
2018-03-27 23:49:44 +0200

    Delete sout from Main.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Main.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Main.java
@@ -5,8 +5,9 @@ public class Main {
         public static void main(String[] args) {
 
                 String filename = args[0];
-                System.out.println(filename);
+
                 ApplicationController appController = new ApplicationController(filename);
-                appController.print();
+
+                appController.results();
         }
 }

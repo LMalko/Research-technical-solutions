33f5609e74f3ed0a9dfdf48437e77d4106c2dbd1
LMalko <lmalko2601@gmail.com>
2018-03-28 00:04:36 +0200

    Delete test code from Application Controller, put first 3 stats

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Controller/ApplicationController.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Controller/ApplicationController.java
@@ -3,25 +3,30 @@ package Controller;
 import Iterator.CharIterator;
 import Model.FileContent;
 import Iterator.WordIterator;
+import Model.StatisticalAnalysis;
 
 public class ApplicationController {
 
         private String filename;
+        private FileContent fileContent;
 
         public ApplicationController(String filename) {
                 this.filename = filename;
+                fileContent = new FileContent(filename);
         }
 
-        public void print(){
-                FileContent fileContent = new FileContent(this.filename);
-                WordIterator wordIterator = new WordIterator(fileContent.textContent());
-                while(wordIterator.hasNext()){
-                        System.out.println(wordIterator.next());
-                }
-                CharIterator charIterator = new CharIterator(fileContent.textContent());
-                while(charIterator.hasNext()){
-                        System.out.println(charIterator.next());
-                }
+        public void results(){
+                System.out.println(String.format("\n\nDOCUMENT %s LEXICAL ANALYSIS: \n\n", this.filename));
+
+                StatisticalAnalysis analysisChar = new StatisticalAnalysis(new CharIterator(fileContent.textContent()));
+                System.out.println(String.format("      Alphanumeric character count: %d", analysisChar.getCount()));
+
+                StatisticalAnalysis analysisWord = new StatisticalAnalysis(new WordIterator(fileContent.textContent()));
+                System.out.println(String.format("      Words count: %d", analysisWord.getCount()));
+
+
+                analysisWord = new StatisticalAnalysis(new WordIterator(fileContent.textContent()));
+                System.out.println(String.format("      Author's Dictionary (distinct words count): %d",analysisWord.dictionarySize()));
         }
 
 }

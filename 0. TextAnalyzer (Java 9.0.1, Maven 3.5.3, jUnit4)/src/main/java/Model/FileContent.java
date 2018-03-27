2287f49c954836280e017c6b0286d3ee64bb3388
LMalko <lmalko2601@gmail.com>
2018-03-27 02:31:44 +0200

    Write methods in FileContent.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Model/FileContent.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Model/FileContent.java
@@ -1,33 +1,46 @@
 package Model;
 
 import Interface.IterableText;
-
-import java.util.ArrayList;
+import Iterator.CharIterator;
+import Iterator.WordIterator;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 
 public class FileContent implements IterableText{
 
-        private String filename;
+        private String fileName;
+        private BufferedReader br;
 
-        public FileContent(String filename){
-                this.filename = filename;
+
+        public FileContent(String fileName){
+                this.fileName = fileName;
         }
         @Override
         public Iterator<String> charIterator() {
-                return new Iterator.CharIterator(this);
+                return new CharIterator(this);
         }
 
         @Override
         public Iterator<String> wordIterator() {
-                return new Iterator.WordIterator(this);
+                return new WordIterator(this);
         }
 
-        public ArrayList<String> getWordsCollection(){
-
+        public String getFilename() {
+                return fileName;
         }
 
-        public ArrayList<String> getCharsCollection(){
-
+        public BufferedReader textContent(){
+                try {
+                        br = new BufferedReader(new FileReader(this.fileName));
+                } catch (FileNotFoundException exceptionFile) {
+                        exceptionFile.printStackTrace();
+                } catch (IOException exceptionIO){
+                        exceptionIO.printStackTrace();
+                }
+                return br;
         }
 
 }

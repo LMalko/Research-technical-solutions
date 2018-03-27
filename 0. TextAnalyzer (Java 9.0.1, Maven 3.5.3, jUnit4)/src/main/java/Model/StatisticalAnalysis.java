3803e25980dda989bb38317c9f8f2c908e5f47c0
LMalko <lmalko2601@gmail.com>
2018-03-27 23:04:56 +0200

    Write methods for StatisticalAnalysis

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Model/StatisticalAnalysis.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Model/StatisticalAnalysis.java
@@ -1,4 +1,63 @@
 package Model;
 
+import java.util.*;
+
 public class StatisticalAnalysis {
+
+        private Iterator<String> iterator;
+
+        public StatisticalAnalysis(Iterator<String> iterator) {
+                this.iterator = iterator;
+        }
+
+        public int getCountOf(String element){
+                int result = 0;
+                while(iterator.hasNext()){
+                        if(iterator.next() == element){
+                                result ++;
+                        }
+                }
+                return result;
+        }
+
+        public int dictionarySize(){
+                Set<String> result = new HashSet<>();
+                while(iterator.hasNext()){
+                        result.add(iterator.next());
+                }
+                return result.size();
+        }
+
+        public int size(){
+                int result = 0;
+                while(iterator.hasNext()){
+                        result ++;
+                }
+                return result;
+        }
+
+        public Set<String> occurMoreThan(Integer number){
+                Map<String, Integer> dictionary = getMapStatistics();
+                HashSet result = new HashSet<>();
+                for(String key: dictionary.keySet()){
+                        if(dictionary.get(key) > number){
+                                result.add(dictionary.get(key));
+                        }
+                }
+                return result;
+        }
+
+        public Map<String, Integer> getMapStatistics(){
+                Map<String, Integer> dictionary = new HashMap<>();
+                while(iterator.hasNext()){
+                        String tempString = iterator.next();
+                        if(dictionary.get(tempString) == null){
+                                dictionary.put(tempString, 1);
+                        }else{
+                                int tempInt = dictionary.get(tempString);
+                                dictionary.put(tempString, tempInt);
+                        }
+                }
+                return dictionary;
+        }
 }

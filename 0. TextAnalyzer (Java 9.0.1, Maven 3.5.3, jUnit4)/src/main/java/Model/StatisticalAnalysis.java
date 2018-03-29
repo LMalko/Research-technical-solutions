f3e22494725b87c17fd804ba95fd7f40ba491c82
LMalko <lmalko2601@gmail.com>
2018-03-29 14:44:24 +0200

    Add sortMapVyValues() to StatisticalAnalysis.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Model/StatisticalAnalysis.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Model/StatisticalAnalysis.java
@@ -45,7 +45,7 @@ public class StatisticalAnalysis {
                         number = 1;
                 }
                 Map<String, Integer> dictionary = getMapStatistics();
-                HashSet result = new HashSet<>();
+                Set result = new LinkedHashSet();
                 for(String key: dictionary.keySet()){
                         if(dictionary.get(key) > number){
                                 result.add(String.format("%s - %d times", key, dictionary.get(key)));
@@ -54,7 +54,7 @@ public class StatisticalAnalysis {
                 return result;
         }
 
-        public Map<String, Integer> getMapStatistics(){
+        private Map<String, Integer> getMapStatistics(){
                 Map<String, Integer> dictionary = new HashMap<>();
                 while(iterator.hasNext()){
                         String tempString = iterator.next();
@@ -65,6 +65,35 @@ public class StatisticalAnalysis {
                                 dictionary.put(tempString, tempInt);
                         }
                 }
-                return dictionary;
+                return sortHashMapByValues(dictionary);
+        }
+
+        private LinkedHashMap<String, Integer> sortHashMapByValues(Map<String, Integer> passedMap) {
+                List<String> mapKeys = new ArrayList<>(passedMap.keySet());
+                List<Integer> mapValues = new ArrayList<>(passedMap.values());
+                Collections.sort(mapValues);
+                Collections.sort(mapKeys);
+
+                LinkedHashMap<String, Integer> sortedMap =
+                        new LinkedHashMap<>();
+
+                Iterator<Integer> valueIt = mapValues.iterator();
+                while (valueIt.hasNext()) {
+                        Integer val = valueIt.next();
+                        Iterator<String> keyIt = mapKeys.iterator();
+
+                        while (keyIt.hasNext()) {
+                                String key = keyIt.next();
+                                Integer comp1 = passedMap.get(key);
+                                Integer comp2 = val;
+
+                                if (comp1.equals(comp2)) {
+                                        keyIt.remove();
+                                        sortedMap.put(key, val);
+                                        break;
+                                }
+                        }
+                }
+                return sortedMap;
         }
 }

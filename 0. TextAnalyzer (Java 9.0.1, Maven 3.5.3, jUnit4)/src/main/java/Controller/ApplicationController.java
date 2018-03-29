4522ae68b7171457a60c077370321c603fdc3b2f
LMalko <lmalko2601@gmail.com>
2018-03-29 15:12:11 +0200

    Call occurMoreThan() in ApplicationController.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Controller/ApplicationController.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Controller/ApplicationController.java
@@ -1,8 +1,8 @@
 package Controller;
 
 import Iterator.CharIterator;
-import Model.FileContent;
 import Iterator.WordIterator;
+import Model.FileContent;
 import Model.StatisticalAnalysis;
 
 public class ApplicationController {
@@ -19,14 +19,28 @@ public class ApplicationController {
                 System.out.println(String.format("\n\nDOCUMENT %s LEXICAL ANALYSIS: \n\n", this.filename));
 
                 StatisticalAnalysis analysisChar = new StatisticalAnalysis(new CharIterator(fileContent.textContent()));
-                System.out.println(String.format("      Alphanumeric character count: %d", analysisChar.getCount()));
+                System.out.println(String.format("\tAlphanumeric character count: %d", analysisChar.getCount()));
 
                 StatisticalAnalysis analysisWord = new StatisticalAnalysis(new WordIterator(fileContent.textContent()));
-                System.out.println(String.format("      Words count: %d", analysisWord.getCount()));
+                System.out.println(String.format("\tWords count: %d", analysisWord.getCount()));
 
 
                 analysisWord = new StatisticalAnalysis(new WordIterator(fileContent.textContent()));
-                System.out.println(String.format("      Author's Dictionary (distinct words count): %d",analysisWord.dictionarySize()));
+                int dictionarySize = analysisWord.dictionarySize();
+                System.out.println(String.format("\tAuthor's Dictionary (distinct words count): %d", dictionarySize));
+
+                analysisWord = new StatisticalAnalysis(new WordIterator(fileContent.textContent()));
+                System.out.println("\tTOP20 words occuring more than once:\n");
+                Object[] topWords = analysisWord.occurMoreThan(1).toArray();
+                int amountOfTopWords = 0;
+                try {
+                        for (int i = 0; i < 20; i++) {
+                                System.out.println(String.format("\t\t%d. %s", i, topWords[i]));
+                                amountOfTopWords ++;
+                        }
+                }catch(ArrayIndexOutOfBoundsException exception){
+                        System.out.println(String.format("\n\t\tOnly %s word(s) occur more than once.", String.valueOf(amountOfTopWords)));
+                }
         }
 
 }

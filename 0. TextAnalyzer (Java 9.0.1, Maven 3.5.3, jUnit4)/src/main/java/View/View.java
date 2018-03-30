a77688c27cccfc39783da1b7be41a18fb522bd5c
LMalko <lmalko2601@gmail.com>
2018-03-30 15:01:02 +0200

    Add print for formatted stings in View.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/View/View.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/View/View.java
@@ -2,6 +2,10 @@ package View;
 
 public class View {
 
+        public void print(String text, Object ... args){
+                System.out.println(String.format(text, args));
+        }
+
         public void print(String text){
                 System.out.println(text);
         }

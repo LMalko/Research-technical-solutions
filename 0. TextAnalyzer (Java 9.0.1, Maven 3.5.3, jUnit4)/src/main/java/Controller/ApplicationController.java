0078cd931089584c7d48b2378dea69869560086a
LMalko <lmalko2601@gmail.com>
2018-03-30 14:48:19 +0200

    Apply View object in ApplicationController.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Controller/ApplicationController.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Controller/ApplicationController.java
@@ -4,11 +4,13 @@ import Iterator.CharIterator;
 import Iterator.WordIterator;
 import Model.FileContent;
 import Model.StatisticalAnalysis;
+import View.View;
 
 public class ApplicationController {
 
         private String filename;
         private FileContent fileContent;
+        private View view = new View();
 
         public ApplicationController(String filename) {
                 this.filename = filename;
@@ -16,7 +18,7 @@ public class ApplicationController {
         }
 
         public void displayResults() {
-                System.out.println(String.format("\n\nDOCUMENT %s LEXICAL ANALYSIS: \n\n", this.filename));
+                view.print(String.format("\n\nDOCUMENT %s LEXICAL ANALYSIS: \n\n", this.filename));
                 displayAllChars();
                 displayAllWords();
                 displayDictionarySize();
@@ -25,36 +27,36 @@ public class ApplicationController {
         }
         private void displayAllChars(){
                 StatisticalAnalysis analysisChar = new StatisticalAnalysis(new CharIterator(fileContent.textContent()));
-                System.out.println(String.format("\t01. Alphanumeric character count: %d", analysisChar.getCount()));
+                view.print(String.format("\t01. Alphanumeric character count: %d", analysisChar.getCount()));
         }
 
         private void displayAllWords(){
                 StatisticalAnalysis analysisWord = new StatisticalAnalysis(new WordIterator(fileContent.textContent()));
-                System.out.println(String.format("\t02. Words count: %d", analysisWord.getCount()));
+                view.print(String.format("\t02. Words count: %d", analysisWord.getCount()));
         }
 
         private void displayDictionarySize(){
                 StatisticalAnalysis analysisWord = new StatisticalAnalysis(new WordIterator(fileContent.textContent()));
                 int dictionarySize = analysisWord.dictionarySize();
-                System.out.println(String.format("\t03. Author's Dictionary (distinct words count): %d", dictionarySize));
+                view.print(String.format("\t03. Author's Dictionary (distinct words count): %d", dictionarySize));
         }
 
         private void displayTop30Words(){
                 StatisticalAnalysis analysisWord = new StatisticalAnalysis(new WordIterator(fileContent.textContent()));
-                System.out.println("\t04. TOP30 words occuring more than once:\n");
+                view.print("\t04. TOP30 words occuring more than once:\n");
                 String[] topWords = analysisWord.occurMoreThan(1).toArray(new String[0]);
                 int amountOfTopWords = 0;
                 try {
                         for (int i = 0; i < 9; i++) {
-                                System.out.println(String.format("\t\t*%d.  %s", i + 1, topWords[i]));
+                                view.print(String.format("\t\t*%d.  %s", i + 1, topWords[i]));
                                 amountOfTopWords ++;
                         }
                         for (int i = 9; i < 30; i++) {
-                                System.out.println(String.format("\t\t*%d. %s", i + 1, topWords[i]));
+                                view.print(String.format("\t\t*%d. %s", i + 1, topWords[i]));
                                 amountOfTopWords ++;
                         }
                 }catch(ArrayIndexOutOfBoundsException exception){
-                        System.out.println(String.format("\n\t\tOnly %s word(s) occur more than once.", String.valueOf(amountOfTopWords)));
+                        view.print(String.format("\n\t\tOnly %s word(s) occur more than once.", String.valueOf(amountOfTopWords)));
                 }
         }
 }

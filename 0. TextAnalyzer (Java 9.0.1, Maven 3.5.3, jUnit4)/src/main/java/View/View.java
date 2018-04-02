a882da7f3eeef7b72177128de9780116a542ec61
LMalko <lmalko2601@gmail.com>
2018-04-02 20:18:25 +0200

    Clean code in View.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/View/View.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/View/View.java
@@ -3,7 +3,7 @@ package View;
 public class View {
 
         public void print(String text, Object ... args){
-                System.out.println(String.format(text, args));
+                System.out.printf(text, args);
         }
 
         public void print(String text){

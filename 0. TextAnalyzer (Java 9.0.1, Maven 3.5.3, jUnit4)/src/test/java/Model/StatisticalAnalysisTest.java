9a902597a0794b1ab9c74d8765c86326c8eafb75
LMalko <lmalko2601@gmail.com>
2018-04-05 13:54:03 +0200

    Add 2 tests to StatisticalAnalysisTest.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/test/java/Model/StatisticalAnalysisTest.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/test/java/Model/StatisticalAnalysisTest.java
@@ -21,14 +21,20 @@ public class StatisticalAnalysisTest {
 
         @Test
         public void getCount() {
+                assertEquals(charAnalysis.getCount(), 955386);
+                assertEquals(wordAnalysis.getCount(), 219043);
+
         }
 
         @Test
         public void dictionarySize() {
+                wordAnalysis = new StatisticalAnalysis(new WordIterator(new FileContent("testText.txt")));
+                assertEquals(wordAnalysis.dictionarySize(), 16959);
         }
 
         @Test
         public void occurMoreThan() {
+                charAnalysis = new StatisticalAnalysis(new CharIterator(new FileContent("testText.txt")));
                 assertTrue(charAnalysis.occurMoreThan(1).get(0).get(0).toString().matches("[a-zA-Z0-9]"));
                 charAnalysis = new StatisticalAnalysis(new CharIterator(new FileContent("testText.txt")));
                 assertTrue(charAnalysis.occurMoreThan(1).get(0).get(1).toString().matches("[0-9]+"));
@@ -36,6 +42,7 @@ public class StatisticalAnalysisTest {
 
         @Test
         public void wordsLenMoreThan4() {
+                wordAnalysis = new StatisticalAnalysis(new WordIterator(new FileContent("testText.txt")));
                 assertTrue(wordAnalysis.wordsLenMoreThan4().get(0).get(0).toString().matches("[a-zA-Z0-9]{4,}"));
                 wordAnalysis = new StatisticalAnalysis(new WordIterator(new FileContent("testText.txt")));
                 assertTrue(wordAnalysis.wordsLenMoreThan4().get(0).get(1).toString().matches("[0-9]+"));

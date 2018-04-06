09f11a75b3b1536aa1eb5a83cd8f353b02d5e0b0
LMalko <lmalko2601@gmail.com>
2018-04-07 01:23:17 +0200

    Delete colors from ApplicationController.java

--- 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Controller/ApplicationController.java
+++ 0. TextAnalyzer (Java 9.0.1, Maven 3.5.3, jUnit4)/src/main/java/Controller/ApplicationController.java
@@ -19,18 +19,12 @@ import java.util.List;
 
 public class ApplicationController {
 
-        private File file;
         private String filename;
         private FileContent fileContent;
+        private View view = new View();
         private StatisticalAnalysis analysisWord;
         private StatisticalAnalysis analysisChar;
-
-        private static final String ANSI_RED = "\u001B[31m";
-        private static final String ANSI_GREEN = "\u001B[32m";
-        private static final String ANSI_CYAN = "\u001B[36m";
-        private static final String ANSI_PURPLE = "\u001B[35m";
-
-        private View view = new View();
+        private File file;
 
         public ApplicationController(String filename) {
                 this.filename = filename;
@@ -72,8 +66,7 @@ public class ApplicationController {
                 long timeStart = System.currentTimeMillis();
                 String date = getDate();
 
-                view.print("\n\n%s DOCUMENT %s LEXICAL ANALYSIS [%s]: \n\n",
-                                  ANSI_GREEN,this.filename, date);
+                view.print("\n\nDOCUMENT %s LEXICAL ANALYSIS [%s]: \n\n", this.filename, date);
                 saveRecordToFile(file, "\n\nDOCUMENT %s LEXICAL ANALYSIS [%s]: \n\n", this.filename, date);
 
                 displayAllChars();
@@ -87,9 +80,8 @@ public class ApplicationController {
                 long timeDelta = timeEnd - timeStart;
                 double elapsedSeconds = timeDelta / 1000.0;
 
-                view.print("\n\n\t%sAnalysis took %f seconds to complete.\n\t%sThis lexical analysis has been saved" +
-                        " to a file in the same directory.", ANSI_GREEN, elapsedSeconds, ANSI_PURPLE);
-
+                view.print("\n\n\tAnalysis took %f seconds to complete.", elapsedSeconds);
+                view.print("\n\tThis lexical analysis has been saved to a file in the same directory.");
                 saveRecordToFile(file, "\n\n\tAnalysis took %f seconds to complete.", elapsedSeconds);
 
         }
@@ -97,7 +89,7 @@ public class ApplicationController {
                 analysisChar = new StatisticalAnalysis(new CharIterator(fileContent));
 
                 int count = analysisChar.getCount();
-                view.print("\t%s01. Alphanumeric character count: %s%d", ANSI_RED, ANSI_CYAN, count);
+                view.print("\t01. Alphanumeric character count: %d", count);
                 saveRecordToFile(file, "\t01. Alphanumeric character count: %d\n", count);
         }
 
@@ -105,7 +97,7 @@ public class ApplicationController {
                 analysisWord = new StatisticalAnalysis(new WordIterator(fileContent));
 
                 int count = analysisWord.getCount();
-                view.print("\t%s02. Words count: %s%d", ANSI_RED, ANSI_CYAN, count);
+                view.print("\t02. Words count: %d", count);
                 saveRecordToFile(file,"\t02. Words count: %d\n", count);
         }
 
@@ -113,15 +105,14 @@ public class ApplicationController {
                 analysisWord = new StatisticalAnalysis(new WordIterator(fileContent));
                 int dictionarySize = analysisWord.dictionarySize();
 
-                view.print("\t%s03. Author's Dictionary (distinct words count): %s%d",
-                                   ANSI_RED, ANSI_CYAN, dictionarySize);
+                view.print("\t03. Author's Dictionary (distinct words count): %d", dictionarySize);
                 saveRecordToFile(file, "\t03. Author's Dictionary (distinct words count): %d\n", dictionarySize);
         }
 
         private void displayTop30Words(){
                 analysisWord = new StatisticalAnalysis(new WordIterator(fileContent));
 
-                view.print("\t%s04. TOP30 words occuring more than once:%s\n", ANSI_RED, ANSI_CYAN);
+                view.print("\t04. TOP30 words occuring more than once:\n");
                 saveRecordToFile(file, "\t04. TOP30 words occuring more than once:\n\n");
 
                 List<List> topWords = analysisWord.occurMoreThan(1);
@@ -129,9 +120,9 @@ public class ApplicationController {
                 try {
                         for (int i = 0; i < 30; i++) {
                                 view.print("\t\t*%02d.  %s - %s times", i + 1,
-                                                  topWords.get(i).get(0), topWords.get(i).get(1));
+                                        topWords.get(i).get(0), topWords.get(i).get(1));
                                 saveRecordToFile(file, "\t\t*%02d.  %s - %s times\n", i + 1,
-                                                              topWords.get(i).get(0), topWords.get(i).get(1));
+                                        topWords.get(i).get(0), topWords.get(i).get(1));
                                 amountOfTopWords ++;
                         }
                 }catch(IndexOutOfBoundsException exception){
@@ -145,8 +136,7 @@ public class ApplicationController {
         private void displayTop30WordsLongerThan4(){
                 analysisWord = new StatisticalAnalysis(new WordIterator(fileContent));
 
-                view.print("\n\t%s05. TOP30 words occuring more than once & longer than 4 letters:%s\n\n",
-                                   ANSI_RED, ANSI_CYAN);
+                view.print("\n\t05. TOP30 words occuring more than once & longer than 4 letters:\n\n");
                 saveRecordToFile(file, "\n\t05. TOP30 words occuring more than once & longer than 4 letters:\n");
 
                 List<List> topWordsMoreThan4 = analysisWord.wordsLenMoreThan4();
@@ -154,9 +144,9 @@ public class ApplicationController {
                 try {
                         for (int i = 0; i < 30; i++) {
                                 view.print("\t\t*%02d.  %s - %s times", i + 1,
-                                                   topWordsMoreThan4.get(i).get(0), topWordsMoreThan4.get(i).get(1));
+                                        topWordsMoreThan4.get(i).get(0), topWordsMoreThan4.get(i).get(1));
                                 saveRecordToFile(file, "\t\t*%02d.  %s - %s times\n", i + 1,
-                                                              topWordsMoreThan4.get(i).get(0), topWordsMoreThan4.get(i).get(1));
+                                        topWordsMoreThan4.get(i).get(0), topWordsMoreThan4.get(i).get(1));
                                 amountOfTopWords ++;
                         }
                 }catch(IndexOutOfBoundsException exception){
@@ -169,19 +159,20 @@ public class ApplicationController {
 
         private void displayCharsInOrder(){
                 analysisChar = new StatisticalAnalysis(new CharIterator(fileContent));
-                view.print("\n\t%s06. Letters & digits in order of number of occurence count: %s\n",
-                                   ANSI_RED, ANSI_CYAN);
+                view.print("\n\t06. Letters & digits in order of number of occurence count: \n");
                 saveRecordToFile(file, "\n\t06. Letters & digits in order of number of occurence count: \n");
                 List<List> orderedChars = analysisChar.occurMoreThan(1);
                 int rankingNUmber = 1;
 
                 for (int i = 0; i < orderedChars.size(); i++) {
                         view.print("\t\t*%02d.  %s - %s times", rankingNUmber, orderedChars.get(i).get(0),
-                                           orderedChars.get(i).get(1));
+                                orderedChars.get(i).get(1));
                         saveRecordToFile(file, "\t\t*%02d.  %s - %s times\n", rankingNUmber++, orderedChars.get(i).get(0),
-                                                      orderedChars.get(i).get(1));
+                                orderedChars.get(i).get(1));
                 }
         }
 }
+
+
 
 

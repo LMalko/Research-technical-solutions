388ad48e49248f51b7a693b3175a3543d4c8983e
LMalko <lmalko2601@gmail.com>
2018-04-16 19:50:43 +0200

    Rename projekt 1

--- 0. TextAnalyzer/src/test/java/MainTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.*;
-
-public class MainTest {
-
-        @org.junit.Test
-        public void main() {
-                assertEquals("Dummy test","Dummy test");
-        }
-}

5274ab00f87754ce18ed0d0f5432dfdc3775f3ed
LMalko <lmalko2601@gmail.com>
2018-04-16 22:52:30 +0200

    Add Main.java

--- 02 Space Simulation/src/main/java/Main.java
+++ 02 Space Simulation/src/main/java/Main.java
@@ -2,10 +2,12 @@ import Controller.ApplicationController;
 
 public class Main {
 
+        private static final String FILENAME = "resources/Intro.txt";
+
         public static void main(String[] args) {
 
-                ApplicationController appController = new ApplicationController();
+                ApplicationController appController = new ApplicationController(FILENAME);
+                appController.startApp();
 
-                appController.startAnalysis();
         }
 }

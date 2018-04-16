40d5d8ce251ca090115a0a26c924c9fa6aa996ac
LMalko <lmalko2601@gmail.com>
2018-04-16 23:46:46 +0200

    Initiat View in ItroController.java

--- 02 Space Simulation/src/main/java/Controller/IntroController.java
+++ 02 Space Simulation/src/main/java/Controller/IntroController.java
@@ -8,7 +8,7 @@ public class IntroController {
 
         private FileContent fileContent;
         private String data;
-        private View view;
+        private View view = new View();
 
         IntroController(String fileName){
                 this.fileContent = new FileContent(fileName);

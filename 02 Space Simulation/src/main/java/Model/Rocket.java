ca7446356287bd31b3b9a65cb9a8656a0dd07ab6
LMalko <lmalko2601@gmail.com>
2018-04-17 00:20:07 +0200

    Add rocket.java

---
+++ 02 Space Simulation/src/main/java/Model/Rocket.java
@@ -0,0 +1,39 @@
+package Model;
+
+import Interface.Spaceship;
+
+public class Rocket implements Spaceship{
+
+        private int costInMlnDollars;
+        private int weighInTonnes;
+        private int maxWeightWithCargoInTonnes;
+        private int cargoCarried;
+
+        public Rocket(int costInMlnDollars, int weighInTonnes, int maxWeightWithCargoInTonnes) {
+                this.cargoCarried = 0;
+                this.costInMlnDollars = costInMlnDollars;
+                this.weighInTonnes = weighInTonnes;
+                this.maxWeightWithCargoInTonnes = maxWeightWithCargoInTonnes;
+        }
+
+        @Override
+        public boolean launch() {
+                return true;
+        }
+
+        @Override
+        public boolean land() {
+                return true;
+        }
+
+        @Override
+        public boolean canCarry(Item item) {
+                return this.maxWeightWithCargoInTonnes - (this.cargoCarried + item.getWeight()) >= 0;
+        }
+
+        @Override
+        public void carry(Item item) {
+                this.cargoCarried += item.getWeight();
+        }
+
+}

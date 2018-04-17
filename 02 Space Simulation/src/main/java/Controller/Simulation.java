0ccce201d685514a2cd959dd2decc1c6156773b0
LMalko <lmalko2601@gmail.com>
2018-04-17 23:42:46 +0200

    Add loadRockets iN Simulation.java

--- 02 Space Simulation/src/main/java/Controller/Simulation.java
+++ 02 Space Simulation/src/main/java/Controller/Simulation.java
@@ -2,7 +2,9 @@ package Controller;
 
 import Model.FileContent;
 import Model.Item;
+import Model.Rocket;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 
 class Simulation {
@@ -24,6 +26,27 @@ class Simulation {
                         String itemWeight = aSource.split("=")[1];
                         contentCollection.add(new Item(itemName, Integer.valueOf(itemWeight)));
                 }
+        }
+
+        public ArrayList<Rocket> loadRockets(int phase, Rocket rocket) throws CloneNotSupportedException {
+                if(phase != 1 && phase != 2){ throw new InvalidParameterException("Only phase 1 or 2 allowed."); };
+
+                int weightOfItems;
+                if(phase == 1){weightOfItems = phaseOneContentCollection.stream().mapToInt(Item::getWeight).sum();}
+                else{weightOfItems = phaseTwoContentCollection.stream().mapToInt(Item::getWeight).sum();}
+
+                int rocketsCargoLoad = rocket.getMaxWeightWithCargoInTonnes();
+                int numberOfFullRockets= weightOfItems / rocketsCargoLoad;
+
+                ArrayList<Rocket> rocketList = new ArrayList<>();
+                for(int i = 0; i < numberOfFullRockets; i++){ rocketList.add(rocket.clone()); }
+                int loadModulo = weightOfItems % rocketsCargoLoad;
+                if(loadModulo != 0) {
+                        Rocket temp = rocket.clone();
+                        temp.setCargoCarried(loadModulo);
+                        rocketList.add(temp);
+                }
+                return rocketList;
         }
 }
 

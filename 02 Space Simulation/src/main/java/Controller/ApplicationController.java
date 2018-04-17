000cba5339bc39bb45540746441bae4445494c01
LMalko <lmalko2601@gmail.com>
2018-04-17 23:42:17 +0200

    Throw exception in startApp() in AppController.java

--- 02 Space Simulation/src/main/java/Controller/ApplicationController.java
+++ 02 Space Simulation/src/main/java/Controller/ApplicationController.java
@@ -1,6 +1,6 @@
 package Controller;
 
-import View.*;
+import View.View;
 
 public class ApplicationController {
 
@@ -12,9 +12,14 @@ public class ApplicationController {
                 introController = new IntroController(filename);
         }
 
-        public void startApp(){
+        public void startApp() throws CloneNotSupportedException {
                 view.clearScreen();
                 introController.displayFileContent();
                 simulation = new Simulation("resources/Phase1.txt", "resources/Phase2.txt");
+//                U4Prototype temp = new U4Prototype();
+//                ArrayList<Rocket> uolist = simulation.loadRockets(2, temp);
+//                for(int i = 0; i < uolist.size(); i++){
+//                        System.out.println(uolist.get(i).getCargoCarried());
+//                }
         }
 }

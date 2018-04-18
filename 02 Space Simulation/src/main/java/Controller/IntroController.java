2248021de85b8b4855189eab9c36bfe65fb5cc2a
LMalko <lmalko2601@gmail.com>
2018-04-18 17:30:29 +0200

    Refactor IntroController.java

--- 02 Space Simulation/src/main/java/Controller/IntroController.java
+++ 02 Space Simulation/src/main/java/Controller/IntroController.java
@@ -2,27 +2,13 @@ package Controller;
 
 import Model.FileContent;
 import View.View;
-import java.util.concurrent.TimeUnit;
 
 public class IntroController {
 
-        private String data;
-        private View view = new View();
-
         IntroController(String fileName){
                 FileContent fileContent = new FileContent(fileName);
-                this.data = fileContent.getDataToString();
-        }
-
-        public void displayFileContent(){
-                for(char character: this.data.toCharArray()) {
-                        view.print(character);
-                        try {
-                                long delay = 3;
-                                TimeUnit.MILLISECONDS.sleep(delay);
-                        } catch (InterruptedException e) {
-                                e.printStackTrace();
-                        }
-                }
+                String data = fileContent.getDataToString();
+                View view = new View();
+                view.delayPrint(data);
         }
 }

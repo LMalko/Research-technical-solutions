fc69c7ad54df42e41cb166524270b4454b1c7667
LMalko <lmalko2601@gmail.com>
2018-04-18 17:31:05 +0200

    Add delayPrint to View.java

--- 02 Space Simulation/src/main/java/View/View.java
+++ 02 Space Simulation/src/main/java/View/View.java
@@ -1,5 +1,7 @@
 package View;
 
+import java.util.concurrent.TimeUnit;
+
 public class View {
 
         public void print(String text, Object ... args){
@@ -10,12 +12,24 @@ public class View {
                 System.out.println(text);
         }
 
-        public void print(char character){
+        private void print(char character){
                 System.out.print(character);
         }
 
         public void clearScreen() {
                 System.out.print("\033[H\033[2J");
                 System.out.flush();
+        }
+
+        public void delayPrint(String text){
+                for(char character: text.toCharArray()) {
+                        print(character);
+                        try {
+                                long delay = 2;
+                                TimeUnit.MILLISECONDS.sleep(delay);
+                        } catch (InterruptedException e) {
+                                e.printStackTrace();
+                        }
+                }
         }
 }

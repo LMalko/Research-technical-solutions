b87846ce1dfec44a66770cfa805b6584a2d04380
LMalko <lmalko2601@gmail.com>
2018-04-18 16:44:38 +0200

    Add assignRocketTypes and groups in AppContr.java

--- 02 Space Simulation/src/main/java/Controller/ApplicationController.java
+++ 02 Space Simulation/src/main/java/Controller/ApplicationController.java
@@ -1,12 +1,25 @@
 package Controller;
 
+import Model.*;
 import View.View;
+
+import java.util.ArrayList;
 
 public class ApplicationController {
 
         private IntroController introController;
         private View view = new View();
         private Simulation simulation;
+
+        private U0 templateU0;
+        private U1 templateU1;
+        private U2 templateU2;
+        private U4Prototype templateU4;
+
+        private ArrayList<Rocket> U0groupPhaseOne;
+        private ArrayList<Rocket> U1groupPhaseOne;
+        private ArrayList<Rocket> U2groupPhaseOne;
+        private ArrayList<Rocket> U4groupPhaseOne;
 
         public ApplicationController(String filename){
                 introController = new IntroController(filename);
@@ -16,10 +29,22 @@ public class ApplicationController {
                 view.clearScreen();
                 introController.displayFileContent();
                 simulation = new Simulation("resources/Phase1.txt", "resources/Phase2.txt");
-//                U4Prototype temp = new U4Prototype();
-//                ArrayList<Rocket> uolist = simulation.loadRockets(2, temp);
-//                for(int i = 0; i < uolist.size(); i++){
-//                        System.out.println(uolist.get(i).getCargoCarried());
-//                }
+                assignRocketTypes();
+                assignRocketGroupsPhaseOne();
+
+        }
+
+        private void assignRocketTypes(){
+                templateU0 = new U0();
+                templateU1 = new U1();
+                templateU2 = new U2();
+                templateU4 = new U4Prototype();
+        }
+
+        private void assignRocketGroupsPhaseOne() throws CloneNotSupportedException {
+                U0groupPhaseOne = simulation.loadRockets(1, templateU0);
+                U1groupPhaseOne = simulation.loadRockets(1, templateU1);
+                U2groupPhaseOne = simulation.loadRockets(1, templateU2);
+                U4groupPhaseOne = simulation.loadRockets(1, templateU4);
         }
 }

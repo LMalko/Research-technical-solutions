99c098c8d458640cb10b23ec537a6f726b86329d
LMalko <lmalko2601@gmail.com>
2018-04-18 17:27:16 +0200

    Add runPhaseTwo and run simulations in AppController.java

--- 02 Space Simulation/src/main/java/Controller/ApplicationController.java
+++ 02 Space Simulation/src/main/java/Controller/ApplicationController.java
@@ -21,6 +21,11 @@ public class ApplicationController {
         private ArrayList<Rocket> U2groupPhaseOne;
         private ArrayList<Rocket> U4groupPhaseOne;
 
+        private ArrayList<Rocket> U0groupPhaseTwo;
+        private ArrayList<Rocket> U1groupPhaseTwo;
+        private ArrayList<Rocket> U2groupPhaseTwo;
+        private ArrayList<Rocket> U4groupPhaseTwo;
+
         public ApplicationController(String filename){
                 introController = new IntroController(filename);
         }
@@ -29,9 +34,12 @@ public class ApplicationController {
                 view.clearScreen();
                 introController.displayFileContent();
                 simulation = new Simulation("resources/Phase1.txt", "resources/Phase2.txt");
+
                 assignRocketTypes();
                 assignRocketGroupsPhaseOne();
+                assignRocketGroupsPhaseTwo();
 
+                runSimulations();
         }
 
         private void assignRocketTypes(){
@@ -46,5 +54,23 @@ public class ApplicationController {
                 U1groupPhaseOne = simulation.loadRockets(1, templateU1);
                 U2groupPhaseOne = simulation.loadRockets(1, templateU2);
                 U4groupPhaseOne = simulation.loadRockets(1, templateU4);
+        }
+
+        private void assignRocketGroupsPhaseTwo() throws CloneNotSupportedException {
+                U0groupPhaseTwo = simulation.loadRockets(2, templateU0);
+                U1groupPhaseTwo = simulation.loadRockets(2, templateU1);
+                U2groupPhaseTwo = simulation.loadRockets(2, templateU2);
+                U4groupPhaseTwo = simulation.loadRockets(2, templateU4);
+        }
+
+        private void runSimulations() throws CloneNotSupportedException {
+                view.print("U0 START \n\n");
+                simulation.getRocketsRequired(U0groupPhaseOne);
+                view.print("U1 START \n\n");
+                simulation.getRocketsRequired(U1groupPhaseOne);
+                view.print("U2 START \n\n");
+                simulation.getRocketsRequired(U2groupPhaseOne);
+                view.print("U4 START \n\n");
+                simulation.getRocketsRequired(U4groupPhaseOne);
         }
 }

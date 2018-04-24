5d62847fbdb1c658af83ffdb80c0efaded7186d5
LMalko <lmalko2601@gmail.com>
2018-04-24 09:28:15 +0200

    Add scanner methods in View.java

--- 03 Customers and sales/src/main/java/View/View.java
+++ 03 Customers and sales/src/main/java/View/View.java
@@ -1,8 +1,12 @@
 package View;
 
+import java.io.Console;
 import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
 public class View {
+
+        private Scanner reader = new Scanner(System.in);
 
         public void print(String text, Object ... args){
                 System.out.println(String.format(text, args));
@@ -14,6 +18,23 @@ public class View {
 
         private void print(char character){
                 System.out.print(character);
+        }
+
+        public String getPassword(){
+                Console console = System.console();
+                char passwordArray[] = console.readPassword("Enter password: ");
+                String password = String.valueOf(passwordArray);
+                return password;
+        }
+        
+        public void closeScanner(){
+                reader.close();
+        }
+
+        public String getLogin(){
+                System.out.println("Enter login: ");
+                String login = reader.nextLine();
+                return login;
         }
 
         public void clearScreen() {

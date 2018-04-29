8ea89a89a65b8f7669fb83524be6d1743f5cb7d8
LMalko <lmalko2601@gmail.com>
2018-04-29 15:44:11 +0200

    Add DatabaseDAOConnection.java

---
+++ 03 Customers and sales/src/main/java/Controller/DatabaseDAOConnection.java
@@ -0,0 +1,56 @@
+package Controller;
+
+import DAO.DBStatementProcessor;
+import Interface.Connectable;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DatabaseDAOConnection implements Connectable{
+
+        private String filename;
+        private Connection connection = null;
+        private Statement statement = null;
+        private ResultSet result;
+
+        public DatabaseDAOConnection(String filename) {
+                this.filename = filename;
+        }
+
+        @Override
+        public DBStatementProcessor getDBStatementProcessor(){
+                return new DBStatementProcessor(connection);
+        }
+
+        @Override
+        public void connectToDatabase() {
+                try {
+                        // Register JDBC driver.
+                        Class.forName("org.sqlite.JDBC");
+                        // Open a connection to database.
+                        connection = DriverManager.getConnection(filename);
+                }catch ( Exception exception ) {
+                        System.err.println( exception.getClass().getName() + ": " + exception.getMessage() );
+                }
+        }
+
+        @Override
+        public void closeDatabase(){
+                try{
+                        if (result != null){
+                                result.close();
+                        }
+                        if (statement != null){
+                                statement.close();
+                        }
+                        if (connection != null){
+                                connection.close();
+                        }
+                }catch(Exception exception){
+                        System.err.println(exception.getClass().getName() + ": " + exception.getMessage() );
+                        System.exit(0);
+                }
+        }
+}

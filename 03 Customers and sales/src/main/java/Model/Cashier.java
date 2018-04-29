dd35636245cee30da795e273596bf33bc16f639d
LMalko <lmalko2601@gmail.com>
2018-04-29 14:06:44 +0200

    Add Cashier.java constructor

--- 03 Customers and sales/src/main/java/Model/Cashier.java
+++ 03 Customers and sales/src/main/java/Model/Cashier.java
@@ -1,4 +1,13 @@
 package Model;
 
 public class Cashier extends User{
+
+        public Cashier(String name, String surname, String password) {
+                super(name, surname, password, "cashier");
+        }
+
+        public Cashier(int id, String name, String surname, String password) {
+                super(name, surname, password, "cashier");
+        }
+
 }

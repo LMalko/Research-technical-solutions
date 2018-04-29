4b0d4d6918dd9e9575ee3720690ea18579fe5f90
LMalko <lmalko2601@gmail.com>
2018-04-29 15:45:11 +0200

    Add personalized contructors in Users

--- 03 Customers and sales/src/main/java/Model/Analyst.java
+++ 03 Customers and sales/src/main/java/Model/Analyst.java
@@ -4,10 +4,13 @@ public class Analyst extends User {
 
         public Analyst(String name, String surname, String password) {
                 super(name, surname, password, "analyst");
+                this.login = String.format("%s.%s@analyst.shop.com", name.toLowerCase(), surname.toLowerCase());
         }
 
         public Analyst(int id, String name, String surname, String password) {
-                super(name, surname, password, "analyst");
+                super(id, name, surname, password, "analyst");
         }
 
 }
+
+

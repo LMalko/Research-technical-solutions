2ca3b35a104c2742bda784d704075ee769a08c44
LMalko <lmalko2601@gmail.com>
2018-06-08 00:38:10 +0200

    Write AbstractProcess class

--- 03 Customers and sales/src/main/java/Model/AbstractProcess.java
+++ 03 Customers and sales/src/main/java/Model/AbstractProcess.java
@@ -1,4 +1,25 @@
 package Model;
 
+import Interface.Orderable;
+
 abstract class AbstractProcess {
+
+        private Orderable item;
+
+        public void process(Orderable item) {
+                this.item = item;
+                stepBefore();
+                action(item);
+                stepAfter();
+        }
+        public void stepBefore(){
+                System.out.println("ORDER STATUS WAS --> " + item.toString() + "\n");
+
+        }
+        protected abstract void action(Orderable item);
+
+        public void stepAfter(){
+                System.out.println("ORDER UPDATE --> " + item.toString());
+        }
+
 }
